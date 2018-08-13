a9cb1bcc160fd1292006d1f777b83804852f9624
xioayuan <402973804@qq.com>
2018-08-13 20:49:23 +0800

    提交jszp的Demo项目

---
+++ JszpDemo/app/src/main/java/com/xiao/jszpdemo/application/JszpApplication.java
@@ -0,0 +1,19 @@
+package com.xiao.jszpdemo.application;
+
+import android.app.Application;
+
+public class JszpApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //初始化okhttp
+        initOkhttp();
+    }
+
+    /**
+     * 初始化okhttp
+     */
+    private void initOkhttp() {
+        new OkGoInitUtils().initOkGo(this);
+    }
+}
