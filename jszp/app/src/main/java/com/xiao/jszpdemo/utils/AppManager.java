caaef7bdae1ec2833b0400b99bd5282034a3d5e2
xiaoyuan916 <402973804@qq.com>
2018-09-12 17:11:15 +0800

    提交activity管理类

---
+++ jszp/app/src/main/java/com/xiao/jszpdemo/utils/AppManager.java
@@ -0,0 +1,148 @@
+package com.xiao.jszpdemo.utils;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+
+import java.util.Stack;
+
+/**
+ * //                                  _oo8oo_
+ * //                                 o8888888o
+ * //                                 88" . "88
+ * //                                 (| -_- |)
+ * //                                 0\  =  /0
+ * //                               ___/'==='\___
+ * //                             .' \\|     |// '.
+ * //                            / \\|||  :  |||// \
+ * //                           / _||||| -:- |||||_ \
+ * //                          |   | \\\  -  /// |   |
+ * //                          | \_|  ''\---/''  |_/ |
+ * //                          \  .-\__  '-'  __/-.  /
+ * //                        ___'. .'  /--.--\  '. .'___
+ * //                     ."" '<  '.___\_<|>_/___.'  >' "".
+ * //                    | | :  `- \`.:`\ _ /`:.`/ -`  : | |
+ * //                    \  \ `-.   \_ __\ /__ _/   .-` /  /
+ * //                =====`-.____`.___ \_____/ ___.`____.-`=====
+ * //                                  `=---=`
+ * //
+ * //
+ * //               ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * //
+ * //                          佛祖保佑         永不宕机/永无bug
+ * Created by Zg on 2017/4/24
+ */
+
+public class AppManager {
+
+    private static Stack<Activity> activityStack;
+    private static AppManager instance;
+
+    private AppManager() {
+    }
+
+    /**
+     * 单一实例
+     */
+    public static synchronized AppManager getAppManager() {
+        if (instance == null) {
+            instance = new AppManager();
+        }
+        return instance;
+    }
+
+    /**
+     * 添加Activity到堆栈
+     */
+    public void addActivity(Activity activity) {
+        if (activityStack == null) {
+            activityStack = new Stack<Activity>();
+        }
+        activityStack.add(activity);
+    }
+
+    /**
+     * 获取当前Activity（堆栈中最后一个压入的）
+     */
+    public Activity currentActivity() {
+        Activity activity = activityStack.lastElement();
+        return activity;
+    }
+
+    /**
+     * 结束当前Activity（堆栈中最后一个压入的）
+     */
+    public void finishActivity() {
+        Activity activity = activityStack.lastElement();
+        finishActivity(activity);
+    }
+
+    /**
+     * 结束指定的Activity
+     */
+    public void finishActivity(Activity activity) {
+        if (activity != null) {
+            activityStack.remove(activity);
+            activity.finish();
+            activity = null;
+        }
+    }
+
+    /**
+     * 结束指定类名的Activity
+     */
+    public void finishActivity(Class<?> cls) {
+        Activity cActivity = null;
+        Stack<Activity> activityStackClone = (Stack<Activity>) activityStack.clone();
+        for (Activity activity : activityStackClone) {
+            if (activity.getClass().equals(cls)) {
+                cActivity = activity;
+                break;
+            }
+        }
+        if(cActivity!=null){
+            finishActivity(cActivity);
+        }
+    }
+
+    /**
+     * 结束除指定类名之外的Activity
+     */
+    public void finishOtherActivity(Class<?> cls) {
+        Stack<Activity> activityStackqqq = new Stack<Activity>();
+        for (Activity activity : activityStack) {
+            if (!activity.getClass().equals(cls)) {
+                activityStackqqq.add(activity);
+                activity.finish();
+            }
+        }
+        activityStack.removeAll(activityStackqqq);
+    }
+
+    /**
+     * 结束所有Activity
+     */
+    public void finishAllActivity() {
+        for (int i = 0, size = activityStack.size(); i < size; i++) {
+            if (null != activityStack.get(i)) {
+                activityStack.get(i).finish();
+            }
+        }
+        activityStack.clear();
+    }
+
+    /**
+     * 退出应用程序
+     */
+    public void AppExit(Context context) {
+        try {
+            finishAllActivity();
+            ActivityManager activityMgr = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+            activityMgr.restartPackage(context.getPackageName());
+            System.exit(0);
+        } catch (Exception e) {
+        }
+    }
+
+
+}
