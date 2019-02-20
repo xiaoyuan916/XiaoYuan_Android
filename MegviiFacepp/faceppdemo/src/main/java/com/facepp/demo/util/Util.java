9f5f36186e03753b69b3d29c36ac5e3e900ddc06
xiaoyuan916 <402973804@qq.com>
2019-02-20 16:16:56 +0800

    提交人脸识别项目

---
+++ MegviiFacepp/faceppdemo/src/main/java/com/facepp/demo/util/Util.java
@@ -0,0 +1,11 @@
+package com.facepp.demo.util;
+
+public class Util {
+
+	//在这边填写 API_KEY 和 API_SECRET
+	public static String API_KEY = "is2r1gvunclccDUBLNJ5p8V-iDWqusCZ";
+	public static String API_SECRET = "C2ANFBMwcrO2DYiAPv_opNxTIeSuPf6e";
+
+	public static String CN_LICENSE_URL = "https://api-cn.faceplusplus.com/sdk/v3/auth";
+	public static String US_LICENSE_URL = "https://api-us.faceplusplus.com/sdk/v3/auth";
+}
