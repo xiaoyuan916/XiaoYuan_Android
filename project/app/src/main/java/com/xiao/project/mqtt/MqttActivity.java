defc83e10253b076c3b718c37ad8017142a3e03a
xiaoyuan916 <402973804@qq.com>
2019-04-23 11:36:56 +0800

    修改使用tcp方式

--- project/app/src/main/java/com/xiao/project/mqtt/MqttActivity.java
+++ project/app/src/main/java/com/xiao/project/mqtt/MqttActivity.java
@@ -1,14 +1,11 @@
 package com.xiao.project.mqtt;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.xiao.project.R;
-import com.xiao.project.websocket.MQTTService;
 
-import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
@@ -24,23 +21,38 @@ public class MqttActivity extends AppCompatActivity {
     }
 
     private void initData() {
+        //注册
+        if (!EventBus.getDefault().isRegistered(this)) {
+            EventBus.getDefault().register(this);
+        }
         initMqttService();
     }
 
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onEvent(MqttMessage message) {
+        try {
+            String msg = new String(message.getPayload());
+            Log.d(getClass().getSimpleName(), msg);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 
     /**
      * 初始化mqttservice
      */
     private void initMqttService() {
-        Intent i = new Intent(this, MQTTService.class);
-        startService(i);
+        MqttService.actionStart(this);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Intent i = new Intent(this, MQTTService.class);
-        stopService(i);
+        MqttService.actionStop(this);
+        //解除注册
+        if (EventBus.getDefault().isRegistered(this)) {
+            EventBus.getDefault().unregister(this);
+        }
     }
 }
