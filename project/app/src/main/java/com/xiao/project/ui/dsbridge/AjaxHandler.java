e8e1fca5e419391e488ca373c61dd0fd8a17aae4
xiaoyuan916 <402973804@qq.com>
2019-05-06 13:58:40 +0800

    修改桥连方式

--- project/app/src/main/java/com/xiao/project/ui/dsbridge/AjaxHandler.java
+++ project/app/src/main/java/com/xiao/project/ui/dsbridge/AjaxHandler.java
@@ -100,7 +100,7 @@ public class AjaxHandler {
                     responseData.put("statusCode",response.code());
                     responseData.put("statusMessage",response.message());
                     Map<String, List<String>> responseHeaders= response.headers().toMultimap();
-                    responseHeaders.remove(null);
+                     responseHeaders.clear();
                     responseData.put("headers",responseHeaders);
                     handler.complete(new JSONObject(responseData).toString());
                 }
